3b08ee1ef8ae2add7be9325be72c20a22fc8ed4a
Rudramani Singha <rgs2151@columbia.edu>
2023-03-22 22:28:33 -0400

    NetRep's Gauss has NaN error for >100 features

--- inspector/rsa/netrep_wrapper.py
+++ inspector/rsa/netrep_wrapper.py
@@ -1,8 +1,6 @@
 
 from inspector import Metric
 from inspector.utility import plot_distmat
-
-from netrep.metrics.stochastic import EnergyStochasticMetric
 from netrep.multiset import pairwise_distances
 
 class NetRep(Metric):
@@ -11,10 +9,8 @@ class NetRep(Metric):
     def __init__(self, verbose = True):
         super().__init__(verbose)
 
-    def score(self, networks = False):
+    def score(self, metric, networks = False):
         '''Compute the netrep score'''
-
-        metric = EnergyStochasticMetric()
         if not networks: networks = self.data['digested_networks']
         distmat_energy = pairwise_distances(metric, networks, verbose=self.verbose)
 
